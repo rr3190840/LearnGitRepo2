366dc8ed508d1efed8ae10f552d6e39083da46eb
Ravi Ranjan <rr3190840@gmail.com>
2020-08-12 12:49:48 +0530

    quotation by EllonMusk

--- HelloWorld.py
+++ HelloWorld.py
@@ -5,3 +5,5 @@ print('Today is bit Rainy')
 print('Our exam date for 8th sem was announced the day before yesterday!')
 
 print('Everyone is ordinary until they choose to be extraordinary!')
+
+print('Hello Everyone')
