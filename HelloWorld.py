0cb0787739d2588bef944c441ab2076e441e6f29
Ravi Ranjan <rr3190840@gmail.com>
2020-08-12 11:30:38 +0530

    In dev branch

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,5 @@
 print('Hi, Ravi how are you?')
 
 print('Today is bit Rainy')
+
+print('Our exam date for 8th sem was announced the day before yesterday!')
--- for_loops.py
+++ for_loops.py
@@ -1,2 +1,4 @@
 for i in range(10):
 	print('Suprabhat!')
+
+print('what about while loop')
