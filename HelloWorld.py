1856735f17bd7b5a4f77b6649c2ad4b87df28b39
Ravi Ranjan <rr3190840@gmail.com>
2020-08-12 11:44:50 +0530

    Modification in remote dev branch

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,3 +3,5 @@ print('Hi, Ravi how are you?')
 print('Today is bit Rainy')
 
 print('Our exam date for 8th sem was announced the day before yesterday!')
+
+print('Everyone is ordinary until they choose to be extraordinary!')
--- for_loops.py
+++ for_loops.py
@@ -2,3 +2,5 @@ for i in range(10):
 	print('Suprabhat!')
 
 print('what about while loop')
+
+print('It cool to start your journey in the morning')
